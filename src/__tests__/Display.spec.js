19e3d3698dc9594bd8bb73ad73b3e3e808bb886a
SabrinaGear <sabrinagear.ft@gmail.com>
2018-12-06 10:53:52 -0600

    display tests

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    shallow(<Display />);
+  });
+
+  it("renders a div called display", () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper.find("div.display").length).toBe(1);
+  });
+
+  it("takes in a string value prop", () => {
+    const wrapper = shallow(<Display />);
+    expect(typeof wrapper.find("div.display").text()).toBe("string");
+  });
+
+  it("renders a passed value prop", () => {
+    const wrapper = shallow(<Display value="5" />);
+    const display = wrapper.find("div.display");
+    expect(display.text()).toBe("5");
   });
 });
